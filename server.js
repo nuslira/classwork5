a01c71d2192ee8b643d6407660c45459fb3a3b13
Nusrat <nusratlira8@gmail.com>
2018-07-11 04:37:37 +0000

    classwork5

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var express = require ('express');
+var app = express();
+var server= http.Server(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+  
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+  
+});
+
+
+
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
