333ce853ec66980ebfd5826f3586ea50f3b05a8f
Nusrat <nusratlira8@gmail.com>
2018-07-30 04:38:23 +0000

    classwork9

--- server.js
+++ server.js
@@ -2,30 +2,46 @@ var http = require('http');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb');
-//for local
-//var db,uri="mongodb://localhost:28017";
-//for c9
-var db,uri="mongodb://"+process.env.IP+":27017";
-
-mongo.MongoClient.connect(uri,
-{userNewUrlParser:true},
-function(err,client){
-  if(err){
-    console.log('could not connect to MongoDB')
-  }else{
-    db= client.db('node-cw8');
-    
-  }
-})
-var save = function(form_data){
-  db.createCollection('users',function (err, collection){});
-  var colection = bd.collection('users');
-  cfollection.save(form_data);
-}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var server = http.Server(app);
+//var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://" + process.env.IP + ":27017/test");
+
+mongoose.connection.on('erroe',function(){
+  console.log('Could not connect to mongodb');
+});
+//define schema with mongoose
+var userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: "Name is required"
+  } ,
+  email:String
+});
+var User = mongoose.model('User',userSchema);
+//for local
+//var db, url = "mongodb://localhost:27071";
+//for c9
+//var db, url = 'mongodb://' + process.env.IP + ":27017/test";
+//mongo.MongoClient.connect(url,
+ //           {useNewUrlParser:true},
+ //           function(err,client){
+ //             if(err){
+  //              console.log(err);
+  //              console.log("Could not connect to mongoDB");
+ //             }else{
+ //               db= client.db('node=cwD');
+//              }
+//            });
+//            
+//var save = function(from_data){
+ // db.createCollection('users',function(err, collection){});
+//  var collection= db.collection('users');
+//  collection.save(from_data);
+//};            
+
 
 
 app.get('/',function(req,res){
@@ -40,11 +56,16 @@ app.get('/email',function(req,res){
 app.post('/submit_user',function(req,res)
 {
     console.log(req.body);
-    save(req.body);
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if(err){
+        return res.status(400)
+                  .json({message:"couldn't save user"})
+      }
+      res.status(200).json(data);
+    })
+    //save(req.body);
     res.status(200);
-});
-app.get('/about',function(req,res){
-  res.sendFile(__dirname+'/about.html');
 });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
